768948070b47cc2308cff8d4c639a6fb4bfa223b
danielzierl <danielzierl@outlook.com>
2022-03-03 18:25:40 +0100

    up

--- src/ppa2/Main.java
+++ src/ppa2/Main.java
@@ -7,13 +7,16 @@ import java.util.Random;
 public class Main {
     DuplicateRemover slowestDR;
     int tenSecMaxN;
+    double finalTime;
 
     public static void main(String[] args) throws Exception {
         Main main = new Main();
+        long t1 = System.currentTimeMillis();
         main.printSlowest();
         main.printTable();
+        long t2 = System.currentTimeMillis();
 
-        System.out.println("All done.");
+        System.out.println("All done. Total runtime: "+((t2-t1)/1000.0)+" seconds");
     }
     /**
      * Generuje nahodna data v rozsahu do 100 000,
@@ -33,7 +36,7 @@ public class Main {
         DuplicateRemover r1= new RemoveDuplicates1();
         DuplicateRemover r2 = new RemoveDuplicates2();
         DuplicateRemover r3 = new RemoveDuplicates3();
-        int dataIncrement = 1000;
+        int dataIncrement = 500;
         double ratio1 = checkXSecond(r1, dataIncrement,1,false);
         double ratio2 = checkXSecond(r2, dataIncrement,1,false);
         double ratio3 = checkXSecondExponentially(r3, dataIncrement,1);
@@ -42,22 +45,24 @@ public class Main {
             slowestDR = r1;
         }
         if (ratio2<ratio1&&ratio2<ratio3){
-            System.out.println("Nejpomalejsi je "+ r3.toString());
+            System.out.println("Nejpomalejsi je "+ r2.toString());
             slowestDR = r2;
         }
         if (ratio3<ratio1&&ratio3<ratio2){
             System.out.println("Nejpomalejsi je "+ r3.toString());
-            slowestDR = r2;
+            slowestDR = r3;
         }
+        System.out.format("Merim %s pro 10 sekund%n", slowestDR.toString());
         checkXSecond(slowestDR, dataIncrement, 10, true);
 
 
 
     }
     public double checkXSecond(DuplicateRemover duplicateRemover, int dataIncrement, int thresholdTimeSeconds,boolean saved){
-        double time;
+        double time=0;
         int data=0;
         int counter=0;
+        try{
         do {
             data= (int) (dataIncrement*counter*counter);
 
@@ -66,25 +71,36 @@ public class Main {
 
         }while (time<thresholdTimeSeconds);
         System.out.format("Metoda %s trvala pres %d sekund (konkretne %f ) pro n = %d \n",duplicateRemover.toString(), thresholdTimeSeconds,time, data);
-        if (saved)tenSecMaxN=data;
+        }catch (OutOfMemoryError ex){
+            System.out.format("Zda se ze dosla pamet pro %s, predbezne zastavuji na %f sekundy pro n = %d %n",duplicateRemover, time, data);
+        }
+        if (saved){
+            tenSecMaxN=data;
+            finalTime=time;
+        }
         return data/time;
 
     }
     public double checkXSecondExponentially(DuplicateRemover duplicateRemover, int dataIncrement,int thresholdTimeSeconds) throws Exception {
-        double time;
+        double time=0;
         int data=0;
         int counter=0;
-        do {
-            data= (int) (dataIncrement*Math.exp(counter));
 
-            time = duplicateRemover.runTimed(generateData(data));
-            counter++;
-            if (counter>100){
-                throw new Exception("HAHA");
-            }
+        try{
+            do {
+                data= (int) (dataIncrement*Math.exp(counter));
 
-        }while (time<thresholdTimeSeconds);
-        System.out.format("Metoda %s trvala pres %d sekund (konkretne %f ) pro n = %d \n",duplicateRemover.toString(), thresholdTimeSeconds, time, data);
+                time = duplicateRemover.runTimed(generateData(data));
+                counter++;
+
+
+            }while (time<thresholdTimeSeconds);
+            System.out.format("Metoda %s trvala pres %d sekund (konkretne %f ) pro n = %d \n",duplicateRemover.toString(), thresholdTimeSeconds, time, data);
+        }catch (OutOfMemoryError ex){
+            System.out.format("Zda se ze dosla pamet pro %s, predbezne zastavuji na %f sekundy pro n = %d %n",duplicateRemover, time, data);
+        }
+
+
         return data/time;
     }
     public void printTable(){
@@ -92,13 +108,17 @@ public class Main {
         DuplicateRemover r2 = new RemoveDuplicates2();
         DuplicateRemover r3 = new RemoveDuplicates3();
         System.out.format("%s\t\t%s  \t\t%s  \t\t%s  \t\t%s  \t\t%s  \t\t%s%n", "n","t1","a1","t2","a2","t3","a3");
+        System.out.println("----------------------------------------------------------------------");
         for (int i = 1000; i <= tenSecMaxN; i+=(tenSecMaxN-1000)/10) {
             int[] generatedData = generateData(i);
+
+
             double t1 = r1.runTimed(generatedData);
             double t2 = r2.runTimed(generatedData);
             double t3 = r3.runTimed(generatedData);
             double slowestTime=0;
 
+            //optimisation
             if (slowestDR instanceof RemoveDuplicates1){
                 slowestTime=t1;
             }
@@ -108,7 +128,7 @@ public class Main {
             if (slowestDR instanceof RemoveDuplicates3){
                 slowestTime=t3;
             }
-            System.out.format("%d\t%f\t%f\t%f\t%f\t%f\t%f%n",i, t1,slowestTime/t1,t2,slowestTime/t2,t3,slowestTime/t3);
+            System.out.format("%d\t|%f\t%f\t%f\t%f\t%f\t%f%n",i, t1,slowestTime/t1,t2,slowestTime/t2,t3,slowestTime/t3);
 
           
         }
